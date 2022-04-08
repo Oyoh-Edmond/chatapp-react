31865bb2310e1a80a53c608a0b9f7b7e76c1b528
Oyoh Edmond <chiwunmba@gmail.com>
2022-04-08 05:28:04 +0100

    text message functioning

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
+const MessageForm = (props) => {
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
 
-const MessageForm = () => {
+    const handleSubmit = (event) =>{
+        event.preventDefault(); //no browser refresh once form is submitted 
+   
+        const text = value.trim();
+
+        // sendMessage is from react-chat-engine
+        if(text.length > 0) sendMessage( creds, chatId, { text } );
+        
+        // reset the text field
+        setValue('');
+    }
+
+    const handleChange = (event) =>{
+        setValue(event.target.value);
+
+        isTyping(props, chatId);
+    }
+
     return ( 
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input 
+                className="message-input" 
+                placeholder="Send a message..." 
+                value={value}
+                onChange={handleChange}    
+                onSubmit = {handleSubmit}   
+            />
+        </form>
     )
 }
 
